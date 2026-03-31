3fa847cdab2d98e4ab8939c3ae69b9703b18a6ae
minsung <flymin00712@gmail.com>
2026-04-01 03:37:08 +0900

    feat : Jump Ability 추가

---
+++ Source/DooDoong/Public/AbilitySystem/Abilities/GA_Jump.h
@@ -0,0 +1,25 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_Jump.generated.h"
+
+
+UCLASS()
+class DOODOONG_API UGA_Jump : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UGA_Jump();
+	
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+	
+};
