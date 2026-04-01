9fe4643320a39a73d19230ed19312976cc2fbbc1
yoonseo <cysyes0403@naver.com>
2026-04-01 21:22:28 +0900

    feat: 타일 이동 Ability Task

---
+++ Source/DooDoong/Public/BoardGame/Abilities/DDMoveTileStep.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "DDMoveTileStep.generated.h"
+
+class ADDTile;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnMoveFinished);
+UCLASS()
+class DOODOONG_API UDDMoveTileStep : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(BlueprintAssignable)
+	FOnMoveFinished OnFinished;
+
+	// Dump: 임시 현재 타일 // 나중에 지울 것
+	// UPROPERTY(EditAnywhere)
+	// ADDTile* CurrentTile;
+	
+	static UDDMoveTileStep* MoveTile(
+		UGameplayAbility* OwningAbility,
+		int32 Steps
+	);
+
+	virtual void Activate() override;
+
+private:
+
+	int32 RemainingSteps;
+
+	void MoveNext();
+};
