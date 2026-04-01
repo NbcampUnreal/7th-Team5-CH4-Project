8a01e795863ad75eb31e7502720f275066fa462d
yoonseo <cysyes0403@naver.com>
2026-04-01 15:36:24 +0900

    Feat: 타일 구조 정의

---
+++ Source/DooDoong/Private/BoardGame/TileManager.cpp
@@ -0,0 +1,48 @@
+﻿#include "BoardGame/TileManager.h"
+#include "EngineUtils.h"
+#include "BoardGame/Tile.h"
+#include "Common/LogManager.h"
+
+
+void ATileManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitializeTiles();
+}
+
+void ATileManager::InitializeTiles()
+{
+	// TileMap 구성
+	for (TActorIterator<ATile> It(GetWorld()); It; ++It)
+	{
+		ATile* Tile = *It;
+		// 이름없는 타일 체크
+		if (Tile->TileRowName.IsNone())
+		{
+			LOG_CYS(Error, TEXT("[TM]TileRowName not set on %s"), *Tile->GetName());
+			continue;
+		}
+		
+		// 중복 타일 체크
+		if (TileMap.Contains(Tile->TileRowName))
+		{
+			LOG_CYS(Error, TEXT("[TM]Duplicate TileRowName: %s"), *Tile->TileRowName.ToString());
+			continue;
+		}
+		
+		TileMap.Add(Tile->TileRowName, Tile);
+	}
+
+	// DataTable 로드
+	for (auto& Pair : TileMap)
+	{
+		Pair.Value->LoadTileData();
+	}
+
+	// NextTiles 연결
+	for (auto& Pair : TileMap)
+	{
+		Pair.Value->ResolveNextTiles(TileMap);
+	}
+}
---
+++ Source/DooDoong/Public/BoardGame/Tile.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/DataTable.h"
+#include "TileData.h"
+#include "Tile.generated.h"
+
+UCLASS()
+class DOODOONG_API ATile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ATile();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+
+	// 컴포넌트
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* Root;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* TileMesh;
+
+	// 캐릭터가 서는 위치
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* StandPoint;
+	
+	// DataTable 참조
+	UPROPERTY(EditAnywhere, Category="Tile")
+	UDataTable* TileDataTable;
+
+	// RowName (현재 타일 ID)
+	UPROPERTY(EditAnywhere, Category="Tile")
+	FName TileRowName;
+
+	// 실제 데이터
+	UPROPERTY(VisibleAnywhere, Category="Tile")
+	FTileRowData TileData;
+
+	UPROPERTY(EditAnywhere, Category="Tile")
+	TArray<ATile*> NextTiles;
+
+	FVector GetStandLocation() const;
+	
+	void LoadTileData();
+	
+	void ResolveNextTiles(const TMap<FName, ATile*>& TileMap);
+};
