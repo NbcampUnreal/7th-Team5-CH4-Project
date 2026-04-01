0da140d8a6114d3ac50b4a006b9115a65d757ded
Daype <mj1527123@gmail.com>
2026-04-01 17:33:26 +0900

    feat : 움직이는 엑터 추가

---
+++ Source/DooDoong/Public/MiniGames/Platformer/Actors/DDDynamicPlatform.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DDDynamicPlatform.generated.h"
+
+class UStaticMeshComponent;
+class USceneComponent;
+
+UCLASS()
+class DOODOONG_API ADDDynamicPlatform : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ADDDynamicPlatform();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	
+public:
+	void MovingPlatform(float DeltaTime);
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Components", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USceneComponent> RootScene;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Components", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> MeshComponent;
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Moving")
+	FVector PlatformVelocity;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= "Moving")
+	float MoveDistance;
+	
+private:
+	FVector StartLocation;
+	FVector MoveDirection;
+};
