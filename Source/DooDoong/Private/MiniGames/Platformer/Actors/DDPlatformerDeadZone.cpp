623de11168764dfc74ab1fe35a752a27cc2d83cc
Daype <mj1527123@gmail.com>
2026-04-01 20:23:05 +0900

    feat : 플레이어 데드존 추가

---
+++ Source/DooDoong/Private/MiniGames/Platformer/Actors/DDPlatformerDeadZone.cpp
@@ -0,0 +1,43 @@
+﻿#include "MiniGames/Platformer/Actors/DDPlatformerDeadZone.h"
+
+#include "Components/BoxComponent.h"
+
+ADDPlatformerDeadZone::ADDPlatformerDeadZone()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
+	
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	BoxCollision->SetupAttachment(RootComponent);
+	BoxCollision->OnComponentBeginOverlap.AddDynamic(this ,&ADDPlatformerDeadZone::OnComponentBeginOverlap);
+}
+
+void ADDPlatformerDeadZone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	StartLocation = FVector(0.0f, 0.0f, 0.0f);
+}
+
+void ADDPlatformerDeadZone::OnComponentBeginOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult
+	)
+{
+	if (OtherActor == nullptr)
+	{
+		return;
+	}
+	
+	OtherActor->SetActorLocation(StartLocation);
+	
+}
+
+
+
---
+++ Source/DooDoong/Public/MiniGames/Platformer/Actors/DDPlatformerDeadZone.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DDPlatformerDeadZone.generated.h"
+
+UCLASS()
+class DOODOONG_API ADDPlatformerDeadZone : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ADDPlatformerDeadZone();
+
+protected:
+	virtual void BeginPlay() override;
+	
+public:
+	UFUNCTION()
+	void OnComponentBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+		);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<class USceneComponent> SceneRoot;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<class UBoxComponent> BoxCollision;
+
+private:
+	FVector StartLocation;
+};
