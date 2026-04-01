01b81f24e23a3ab716526047574411f6062c80d0
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-04-01 16:28:50 +0900

    feat: 미니게임관련 Native Tags를 담은 namespace 추가
    DDMiniGameGameplayTags
    - 상태 태그
      - Idle
      - Preparing
      - Traveling
      - Playing
      - Finishing
      - Returning
      - Completed
    - 장르 태그
      - None
      - Competition
      - Survival
      - Race
    - 종료 조건 태그
      - TimeOver
      - Completed

--- Source/DooDoong/Private/System/DDGameplayTags.cpp
+++ Source/DooDoong/Private/System/DDGameplayTags.cpp
@@ -11,3 +11,25 @@ namespace DDGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(Input_Ability_Jump, "Input.Ability.Jump"); 
 
 }
+
+namespace DDMiniGameGameplayTags
+{
+	// 미니게임 상태 태그
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Idle, "MiniGame.State.Idle", "미니게임 세션이 없고 대기 중인 상태");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Preparing, "MiniGame.State.Preparing", "미니게임 시작을 준비하는 상태");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Traveling, "MiniGame.State.Traveling", "플레이어가 미니게임 맵으로 이동 중인 상태");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Playing, "MiniGame.State.Playing", "미니게임이 현재 진행 중인 상태");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Finishing, "MiniGame.State.Finishing", "미니게임 결과를 정리하고 종료 처리 중인 상태");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Returning, "MiniGame.State.Returning", "플레이어가 원래 맵으로 복귀 중인 상태");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Completed, "MiniGame.State.Completed", "미니게임이 완료된 상태");
+
+	// 미니게임 장르 태그
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Genre_None, "MiniGame.Genre.None", "지정되지 않은 미니게임 장르");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Genre_Competition, "MiniGame.Genre.Competition", "경쟁 미니게임 장르");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Genre_Survival, "MiniGame.Genre.Survival", "생존 미니게임 장르");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Genre_Race, "MiniGame.Genre.Race", "레이스 미니게임 장르");
+
+	// 미니게임 종료 조건 태그
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(FinishReason_TimeOver, "MiniGame.FinishReason.TimeOver", "제한 시간이 종료되어 미니게임이 끝난 경우");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(FinishReason_Completed, "MiniGame.FinishReason.Completed", "승리 조건이나 완료 조건을 충족해 미니게임이 끝난 경우");
+}
--- Source/DooDoong/Public/System/DDGameplayTags.h
+++ Source/DooDoong/Public/System/DDGameplayTags.h
@@ -12,3 +12,25 @@ namespace DDGameplayTags
 	// Ability Input Tags 
 	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Ability_Jump); 
 }
+
+namespace DDMiniGameSystemTags
+{
+	// 미니게임 상태 태그
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Idle);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Preparing);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Traveling);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Playing);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Finishing);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Returning);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Completed);
+
+	// 미니게임 장르 태그
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Genre_None);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Genre_Competition);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Genre_Survival);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Genre_Race);
+
+	// 미니게임 종료 조건 태그
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(FinishReason_TimeOver);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(FinishReason_Completed);
+}
