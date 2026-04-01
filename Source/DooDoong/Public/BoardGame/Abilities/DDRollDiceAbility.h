0aeb0478a66045ffb6051c94d116b0ca2af7f54c
yoonseo <cysyes0403@naver.com>
2026-04-01 21:20:04 +0900

    feat: 주사위 굴리기

---
+++ Source/DooDoong/Public/BoardGame/Abilities/DDRollDiceAbility.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "DDRollDiceAbility.generated.h"
+
+
+UCLASS()
+class DOODOONG_API UDDRollDiceAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+};
