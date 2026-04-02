e9a37ebc2b65db7119e9b924b6b58258039abb97
yoonseo <cysyes0403@naver.com>
2026-04-02 17:33:13 +0900

    feat: 캐릭터 자연스러운 이동

--- Source/DooDoong/Private/BoardGame/Abilities/DDMoveTileStepTask.cpp
+++ Source/DooDoong/Private/BoardGame/Abilities/DDMoveTileStepTask.cpp
@@ -1,6 +1,5 @@
 ﻿#include "BoardGame/Abilities/DDMoveTileStepTask.h"
 #include "BoardGame/DDTile.h"
-#include "Base/Character/DDBaseCharacter.h"
 #include "Common/DDLogManager.h"
 
 UDDMoveTileStepTask* UDDMoveTileStepTask::MoveTile(UGameplayAbility* OwningAbility, int32 Steps)
@@ -30,7 +29,7 @@ void UDDMoveTileStepTask::MoveNext()
 		return;
 	}
 
-	ADDBaseCharacter* Character = Cast<ADDBaseCharacter>(GetAvatarActor());
+	ADDBoardGameCharacter* Character = Cast<ADDBoardGameCharacter>(GetAvatarActor());
 	if (!Character)
 	{
 		LOG_CYS(Warning,TEXT("[MT]NoCharacter"));
@@ -38,11 +37,6 @@ void UDDMoveTileStepTask::MoveNext()
 	}
 
 	// TODO: 캐릭터 State 에서 현재 타일 위치 가져오기
-	// UBoardStateComponent* BoardComp = Character->FindComponentByClass<UBoardStateComponent>();
-	// if (!BoardComp) return;
-
-	// ATile* CurrentTile = BoardComp->GetCurrentTile();
-	// if (!CurrentTile) return;
 	
 	// Dump: 에디터에서 현재 타일 임시 지정
 	ADDTile* CurrentTile = Character->StartTile;
@@ -64,15 +58,28 @@ void UDDMoveTileStepTask::MoveNext()
 	ADDTile* NextTile = CurrentTile->NextTiles[0]; // TODO: 분기 선택
 
 	LOG_CYS(Warning,TEXT("[MT]NEXT:%s"),*NextTile->TileRowName.ToString());
+	
 	// 위치 이동 (텔레포트 → 나중에 보간 가능)
-	Character->SetActorLocation(NextTile->GetStandLocation());
-
+	// Character->SetActorLocation(NextTile->GetStandLocation());
+	Character->MoveToLocation(NextTile->GetStandLocation(Character));
+	
 	// TODO: 캐릭터 State 현재 타일 위치 업데이트
 	// BoardComp->SetCurrentTile(NextTile);
 	Character->StartTile=NextTile;
 
+	// 다음 스텝
+	// GetWorld()->GetTimerManager().SetTimerForNextTick(this, &UDDMoveTileStepTask::MoveNext);
+	Character->OnMoveFinished.AddUniqueDynamic(this,&UDDMoveTileStepTask::OnCharacterMoveFinished);
+}
+void UDDMoveTileStepTask::OnCharacterMoveFinished()
+{
+	ADDBoardGameCharacter* Character = Cast<ADDBoardGameCharacter>(GetAvatarActor());
+	if (!Character) return;
+
+	// Delegate 제거 (중복 방지)
+	Character->OnMoveFinished.RemoveDynamic(this, &UDDMoveTileStepTask::OnCharacterMoveFinished);
+
 	RemainingSteps--;
 
-	// 다음 스텝
-	GetWorld()->GetTimerManager().SetTimerForNextTick(this, &UDDMoveTileStepTask::MoveNext);
+	MoveNext();
 }
--- Source/DooDoong/Public/BoardGame/Abilities/DDMoveTileStepTask.h
+++ Source/DooDoong/Public/BoardGame/Abilities/DDMoveTileStepTask.h
@@ -2,11 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "Abilities/Tasks/AbilityTask.h"
+#include "BoardGame/Character/DDBoardGameCharacter.h"
 #include "DDMoveTileStepTask.generated.h"
 
 class ADDTile;
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnMoveFinished);
 UCLASS()
 class DOODOONG_API UDDMoveTileStepTask : public UAbilityTask
 {
@@ -16,7 +16,7 @@ public:
 
 	UPROPERTY(BlueprintAssignable)
 	FOnMoveFinished OnFinished;
-
+	
 	// Dump: 임시 현재 타일 // 나중에 지울 것
 	// UPROPERTY(EditAnywhere)
 	// ADDTile* CurrentTile;
@@ -33,4 +33,7 @@ private:
 	int32 RemainingSteps;
 
 	void MoveNext();
+public:
+	UFUNCTION()
+	void OnCharacterMoveFinished();
 };
