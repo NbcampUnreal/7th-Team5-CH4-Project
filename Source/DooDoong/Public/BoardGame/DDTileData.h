8c32405195cc2d20b2a17beaade831565c83ac08
yoonseo <cysyes0403@naver.com>
2026-04-02 20:11:17 +0900

    fix: struct 기본값 지정

--- Source/DooDoong/Public/BoardGame/DDTileData.h
+++ Source/DooDoong/Public/BoardGame/DDTileData.h
@@ -4,22 +4,27 @@
 #include "DDTileData.generated.h"
 
 UENUM(BlueprintType)
-enum class ETileType : uint8 {
+enum class ETileType : uint8
+{
 	Normal,
-	Start, 
+	Start,
 	Goal,
-	Move, 
-	Coin, 
-	Item, 
+	Move,
+	Coin,
+	Item,
 }; // 기본, 시작, 목표, 이동, 코인, 아이템 
 
 USTRUCT(BlueprintType)
-struct FTileRowData : public FTableRowBase {
+struct FTileRowData : public FTableRowBase
+{
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere) ETileType TileType;
-	UPROPERTY(EditAnywhere) int32 MoveEffect;
-	UPROPERTY(EditAnywhere) int32 CoinEffect;
+	UPROPERTY(EditAnywhere)
+	ETileType TileType = ETileType::Normal;
+	UPROPERTY(EditAnywhere)
+	int32 MoveEffect = 0;
+	UPROPERTY(EditAnywhere)
+	int32 CoinEffect = 0;
 
 	UPROPERTY(EditAnywhere)
 	TArray<FName> NextTiles;
