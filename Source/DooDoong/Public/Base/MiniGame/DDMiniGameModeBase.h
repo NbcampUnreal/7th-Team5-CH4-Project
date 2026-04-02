32c511d88d38e41f0b08e70acaa5fdb7034ef2b5
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-04-02 20:21:13 +0900

    feat: 미니게임 참가자를 SpawnPoint로 스폰되도록 하는 로직 추가
    Seamless Travel 기능을 활용해서 ServerTravel()로 이동할 때
    PlayerController와 PlayerState가 유지되도록 했고
    
    DDMiniGameSpawnPoint에 SlotIndex 값을 부여해서 플레이어의 SlotIndex와 매칭되는 액터의 위치에 미니게임 내부에서 사용하는 Pawn을 생성해서 컨트롤러를 빙의하는 방식으로 구성

--- Source/DooDoong/Public/Base/MiniGame/DDMiniGameModeBase.h
+++ Source/DooDoong/Public/Base/MiniGame/DDMiniGameModeBase.h
@@ -6,6 +6,7 @@
 #include "GameFramework/GameModeBase.h"
 #include "DDMiniGameModeBase.generated.h"
 
+class ADDMiniGameSpawnPoint;
 class UDDMiniGameRuleSet;
 struct FTimerHandle;
 /**
@@ -20,6 +21,9 @@ public:
 	ADDMiniGameModeBase();
 	
 	virtual void BeginPlay() override;
+	
+	/** Seamless Travel 직후 플레이어별 Pawn과 시작 위치를 공통으로 처리하기 위한 전용 함수 */
+	virtual void HandleSeamlessTravelPlayer(AController*& C) override;
 	
 public:
 	/** 미니게임 데이터 Initialize */
@@ -52,10 +56,39 @@ public:
 	void FinishGame(FGameplayTag Reason);
 
 protected:
+	/** Seamless Travel 시점에도 BeginPlay 전에 참가자 정보가 필요할 수 있어 미니게임 데이터를 로드 */
+	void LoadActiveMiniGameData();
+	
+	/** 현재 PlayerState에 대응되는 참가자 정보를 찾음 */
+	const FMiniGameParticipantInfo* FindParticipantInfo(const APlayerState* PlayerState) const;
+	
+	/** 슬롯 번호에 대응되는 스폰 포인트를 찾음 */
+	ADDMiniGameSpawnPoint* FindSpawnPointBySlotIndex(int32 SlotIndex) const;
+	
+	/** 관전자 처리를 미니게임별로 쉽게 교체할 수 있도록 분리 */
+	virtual bool ShouldSpawnAsSpectator(const FMiniGameParticipantInfo& Participant) const;
+	
+	/** 미니게임별 Pawn 타입 교체를 쉽게 하기 위한 함수 */
+	virtual TSubclassOf<APawn> ResolvePawnClass(const FMiniGameParticipantInfo& Participant) const;
+	
+	/** 기본은 SpawnPoint 액터의 위치에서 스폰되도록, 특수한 경우라면 override 하면 되도록 분리 */
+	virtual FTransform ResolveSpawnTransform(const FMiniGameParticipantInfo& Participant);
+	
+	/** 참가자용 Pawn 스폰 및 Possess 공통 처리 */
+	UFUNCTION(BlueprintCallable, Category="MiniGame|Spawn")
+	virtual void SpawnParticipantPawn(APlayerController* PlayerController, const FMiniGameParticipantInfo& Participant);
+	
+	/** 비참가자 혹은 연결이 끊기거나 여타 예외상황이 발생한 플레이어용 Spectator 스폰*/
+	UFUNCTION(BlueprintCallable, Category="MiniGame|Spawn")
+	virtual void SpawnSpectatorPawn(APlayerController* PlayerController);
+	
+protected:
 	/** MiniGameStateBase를 가져오는 헬퍼 */
 	TObjectPtr<ADDMiniGameStateBase> GetMiniGameState() const { return GetGameState<ADDMiniGameStateBase>(); }
+	
 	/** RuleSet이 있는 경우 Initialize하는 헬퍼 */
 	void InitializeRuleSet();
+	
 	/** 타이머 주기마다 미니게임 시간을 갱신하는 헬퍼 */
 	void UpdateMiniGameTime();
 	
