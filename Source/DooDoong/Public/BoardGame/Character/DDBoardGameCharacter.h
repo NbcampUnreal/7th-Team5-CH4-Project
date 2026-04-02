7a1b4847a1d33959801ee549ee7bdfbc13799f56
yoonseo <cysyes0403@naver.com>
2026-04-02 17:17:58 +0900

    feat: 보드게임 캐릭터 이동 함수

--- Source/DooDoong/Public/BoardGame/Character/DDBoardGameCharacter.h
+++ Source/DooDoong/Public/BoardGame/Character/DDBoardGameCharacter.h
@@ -4,7 +4,9 @@
 #include "Base/Character/DDBaseCharacter.h"
 #include "DDBoardGameCharacter.generated.h"
 
-class UDDBoardGameAttributeSet; 
+class UDDBoardGameAttributeSet;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnMoveFinished);
 
 UCLASS()
 class DOODOONG_API ADDBoardGameCharacter : public ADDBaseCharacter
@@ -13,6 +15,27 @@ class DOODOONG_API ADDBoardGameCharacter : public ADDBaseCharacter
 	
 public:
 	ADDBoardGameCharacter(); 
+	
+	UFUNCTION(BlueprintCallable)
+	void MoveToLocation(FVector TargetLocation);
+	
+	UFUNCTION(BlueprintCallable)
+	void UpdateMove();
+	
+	UPROPERTY()
+	FTimerHandle MoveTimerHandle;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnMoveFinished OnMoveFinished;
+
+private:
+	FVector MoveStartLocation;
+	FVector MoveTargetLocation;
+
+	float MoveElapsedTime = 0.f;
+	float MoveDuration = 0.3f;
+
+	bool bIsMoving = false;
 	
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Attribute", meta = (AllowPrivateAccess = "true"))
