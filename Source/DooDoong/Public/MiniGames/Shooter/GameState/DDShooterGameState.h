c9d449f5e941981af30322f127dddda91f1e4383
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-04-03 21:44:43 +0900

    feat: 슈터게임 GameMode, GameState

---
+++ Source/DooDoong/Public/MiniGames/Shooter/GameState/DDShooterGameState.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/MiniGame/DDMiniGameStateBase.h"
+#include "DDShooterGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOODOONG_API ADDShooterGameState : public ADDMiniGameStateBase
+{
+	GENERATED_BODY()
+};
