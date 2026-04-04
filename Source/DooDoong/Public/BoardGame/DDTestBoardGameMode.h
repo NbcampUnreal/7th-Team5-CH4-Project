90b0cde610ed8200359e063acbe345281a2a6987
yoonseo <cysyes0403@naver.com>
2026-04-04 14:33:46 +0900

    feat: 테스트 보드게임모드
    보드게임 초기 셋팅 로직.. 나중에 보드게임 게임모드 만들어지면 그쪽으로 코드 이동 필요함.

---
+++ Source/DooDoong/Public/BoardGame/DDTestBoardGameMode.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/Game/DDGameModeBase.h"
+#include "DDTestBoardGameMode.generated.h"
+
+
+UCLASS()
+class DOODOONG_API ADDTestBoardGameMode : public ADDGameModeBase
+{
+	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+};
