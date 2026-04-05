589fa2dfb95b2fe5b8796aa1e7acbd4cf4958634
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-04-06 04:49:06 +0900

    feat: 슈팅 미니 게임 토대
    미니게임의 베이스가 되는 클래스들을 생성
    
    주요 추가 목록
    - GA_Shot : 슈팅게임 전용 발사 어빌리티
    - DDShotProjectile(Actor) : 발사체 액터
      - ProjectileMovementComponent의 기본 설정

---
+++ Source/DooDoong/Private/MiniGames/Shooter/Actors/DDShooterCharacter.cpp
@@ -0,0 +1,12 @@
+﻿#include "MiniGames/Shooter/Actors/DDShooterCharacter.h"
+
+ADDShooterCharacter::ADDShooterCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ADDShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/DooDoong/Public/MiniGames/Shooter/Actors/DDShooterCharacter.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/Character/DDBaseCharacter.h"
+#include "DDShooterCharacter.generated.h"
+
+UCLASS()
+class DOODOONG_API ADDShooterCharacter : public ADDBaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ADDShooterCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+};
