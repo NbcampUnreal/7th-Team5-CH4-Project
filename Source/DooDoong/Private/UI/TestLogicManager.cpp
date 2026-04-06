7e0c4aadac20aa9e3f36cb6b8a0692a796084e7e
maggik2048 <maggik2048@gmail.com>
2026-04-06 09:28:30 -0700

    chore : lovely name is binded

--- Source/DooDoong/Private/UI/TestLogicManager.cpp
+++ Source/DooDoong/Private/UI/TestLogicManager.cpp
@@ -5,25 +5,25 @@ void UTestLogicManager::InitTestPlayers()
     Players.Empty();
 
     FTestPlayerData P1;
-    P1.Name = "A";
+    P1.Name = "changmin";
     P1.Gold = 100;
     P1.Health = 80;
     P1.ProfileImage = LoadObject<UTexture2D>(nullptr, TEXT("/Game/DooDoong/05_UI/HUD/UI_Assets/PlayerProfile/p1.p1"));
 
     FTestPlayerData P2;
-    P2.Name = "B";
+    P2.Name = "duckling";
     P2.Gold = 50;
     P2.Health = 100;
     P2.ProfileImage = LoadObject<UTexture2D>(nullptr, TEXT("/Game/DooDoong/05_UI/HUD/UI_Assets/PlayerProfile/p2.p2"));
 
     FTestPlayerData P3;
-    P3.Name = "C";
+    P3.Name = "baby";
     P3.Gold = 200;
     P3.Health = 60;
     P3.ProfileImage = LoadObject<UTexture2D>(nullptr, TEXT("/Game/DooDoong/05_UI/HUD/UI_Assets/PlayerProfile/p3.p3"));
 
     FTestPlayerData P4;
-    P4.Name = "D";
+    P4.Name = "mommy";
     P4.Gold = 10;
     P4.Health = 90;
     P4.ProfileImage = LoadObject<UTexture2D>(nullptr, TEXT("/Game/DooDoong/05_UI/HUD/UI_Assets/PlayerProfile/p4.p4"));
