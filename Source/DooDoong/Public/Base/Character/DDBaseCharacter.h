c8bf70fd44db53e4e66529b11f4337f9928d99bb
yoonseo <cysyes0403@naver.com>
2026-04-06 15:42:44 +0900

    bugfix: 헤더 순환 참조 제거

--- Source/DooDoong/Public/Base/Character/DDBaseCharacter.h
+++ Source/DooDoong/Public/Base/Character/DDBaseCharacter.h
@@ -3,7 +3,6 @@
 #include "CoreMinimal.h"
 #include "AbilitySystemInterface.h"
 #include "GameFramework/Character.h"
-#include "BoardGame/DDTile.h"
 #include "Data/DDAbilitySet.h"
 #include "DDBaseCharacter.generated.h"
 
