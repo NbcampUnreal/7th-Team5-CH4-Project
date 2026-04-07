5d659c102f7f3cbc3233db1923629a4d5e51a381
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-04-07 21:48:57 +0900

    feat: 타겟 액터

---
+++ Source/DooDoong/Private/MiniGames/Shooter/Actors/DDShooterTarget.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MiniGames/Shooter/Actors/DDShooterTarget.h"
+
+
+// Sets default values
+ADDShooterTarget::ADDShooterTarget()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ADDShooterTarget::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADDShooterTarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
