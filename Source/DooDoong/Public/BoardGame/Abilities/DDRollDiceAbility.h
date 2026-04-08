02adb65db01032c285160534d5ed152027571c74
yoonseo <cysyes0403@naver.com>
2026-04-08 18:26:46 +0900

    refactor: 불필요한 코드 정리 및 변수명 수정

--- Source/DooDoong/Public/BoardGame/Abilities/DDRollDiceAbility.h
+++ Source/DooDoong/Public/BoardGame/Abilities/DDRollDiceAbility.h
@@ -12,22 +12,36 @@ class DOODOONG_API UDDRollDiceAbility : public UGameplayAbility
 
 public:
 	UDDRollDiceAbility();
-	
+
 	virtual void ActivateAbility(
 		const FGameplayAbilitySpecHandle Handle,
 		const FGameplayAbilityActorInfo* ActorInfo,
 		const FGameplayAbilityActivationInfo ActivationInfo,
 		const FGameplayEventData* TriggerEventData
 	) override;
-	
+
+	virtual bool CanActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayTagContainer* SourceTags = nullptr,
+		const FGameplayTagContainer* TargetTags = nullptr,
+		FGameplayTagContainer* OptionalRelevantTags = nullptr
+	) const override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility, bool bWasCancelled
+	) override;
+
+protected:
 	UFUNCTION()
 	void OnDiceAnimationFinished();
 
-	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
 	UFUNCTION()
 	void OnMoveFinished();
-	
+
 private:
-	bool bAlreadyRolled=false;
-	int32 CachedDice;
+	int32 DiceResult = 0;
 };
