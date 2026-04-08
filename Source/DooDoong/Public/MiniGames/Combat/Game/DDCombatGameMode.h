fcf59c7a58070159520cc45d69452ab2924c9419
minsung <flymin00712@gmail.com>
2026-04-08 12:21:47 +0900

    feat : 컴뱃 미니게임 모드 추가

---
+++ Source/DooDoong/Public/MiniGames/Combat/Game/DDCombatGameMode.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/Game/DDGameModeBase.h"
+#include "DDCombatGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOODOONG_API ADDCombatGameMode : public ADDGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void OnCharacterKilled(AActor* Killer, AActor* Victim) override;
+};
