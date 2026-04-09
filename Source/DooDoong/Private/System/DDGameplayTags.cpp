0945a46345423f325767c0d9f7d0c66dbe0d0548
yoonseo <cysyes0403@naver.com>
2026-04-09 20:56:41 +0900

    feat: 클릭 Input Tag 추가

--- Source/DooDoong/Private/System/DDGameplayTags.cpp
+++ Source/DooDoong/Private/System/DDGameplayTags.cpp
@@ -6,6 +6,7 @@ namespace DDGameplayTags
 	// Native Input Tags 
 	UE_DEFINE_GAMEPLAY_TAG(Input_Native_Move, "Input.Native.Move");
 	UE_DEFINE_GAMEPLAY_TAG(Input_Native_Look, "Input.Native.Look");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Native_Click, "Input.Native.Click");
 	
 	// Ability Input Tags 
 	UE_DEFINE_GAMEPLAY_TAG(Input_Ability_Jump, "Input.Ability.Jump"); 
