bb737efab48cc1d931d157aef50e6460d5d42d3c
yoonseo <cysyes0403@naver.com>
2026-04-09 20:52:05 +0900

    feat: 화살표 엑터

---
+++ Source/DooDoong/Private/BoardGame/DDSelectableTileActor.cpp
@@ -0,0 +1,24 @@
+﻿#include "BoardGame/DDSelectableTileActor.h"
+#include "Common/DDLogManager.h"
+
+ADDSelectableTileActor::ADDSelectableTileActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Root
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	// Mesh
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Root);
+
+	Mesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Mesh->SetCollisionResponseToAllChannels(ECR_Block);
+	Mesh->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block); // 클릭
+
+	bReplicates = true;
+	TargetTile = nullptr;
+	SetReplicateMovement(true);
+	LOG_CYS(Error, TEXT("화살표 생성"));
+}
---
+++ Source/DooDoong/Public/BoardGame/DDSelectableTileActor.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DDSelectableTileActor.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTileSelected, ADDTile*, SelectedTile);
+
+class ADDTile;
+
+UCLASS()
+class DOODOONG_API ADDSelectableTileActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ADDSelectableTileActor();
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnTileSelected OnTileSelected;
+	
+	ADDTile* TargetTile;
+private:
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* Root;
+	
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+};
