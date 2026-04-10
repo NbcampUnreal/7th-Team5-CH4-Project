70dba7563d20ae167b536ed26df3c1d5051e2adf
yoonseo <cysyes0403@naver.com>
2026-04-10 16:37:13 +0900

    feat: 캐치 미니게임 베이스

---
+++ Source/DooDoong/Private/MiniGames/Catch/GameState/DDCatchGameState.cpp
@@ -0,0 +1,17 @@
+﻿#include "MiniGames/Catch/GameState/DDCatchGameState.h"
+#include "Common/DDLogManager.h"
+#include "Kismet/GameplayStatics.h"
+
+ADDCatchGameState::ADDCatchGameState()
+{
+}
+
+void ADDCatchGameState::Multicast_SetGameCamera_Implementation(AActor* CameraActor)
+{
+	APlayerController* PC = UGameplayStatics::GetPlayerController(this, 0);
+	if (!PC || !CameraActor) return;
+
+	PC->SetViewTargetWithBlend(CameraActor, 0.5f);
+	LOG_CYS(Warning,TEXT("[GS] 게임 카메라 전환"));
+}
+
---
+++ Source/DooDoong/Public/MiniGames/Catch/GameMode/DDCatchGameMode.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/MiniGame/DDMiniGameModeBase.h"
+#include "DDCatchGameMode.generated.h"
+
+UCLASS()
+class DOODOONG_API ADDCatchGameMode : public ADDMiniGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	ADDCatchGameMode();
+	
+	virtual void StartMiniGame() override;
+};
