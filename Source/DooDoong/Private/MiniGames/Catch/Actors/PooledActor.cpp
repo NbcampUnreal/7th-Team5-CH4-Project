407753b84224b2b623e6f97440fc5642a77e0bee
yoonseo <cysyes0403@naver.com>
2026-04-13 10:52:21 +0900

    feat: 오브젝트 풀링
    ^ Conflicts:
    ^	Config/DefaultEngine.ini

---
+++ Source/DooDoong/Private/MiniGames/Catch/Actors/PooledActor.cpp
@@ -0,0 +1,30 @@
+﻿#include "MiniGames/Catch/Actors/PooledActor.h"
+
+
+APooledActor::APooledActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+}
+
+void APooledActor::SetInUse(bool InUse)
+{
+	bInUse = InUse;
+	// 콜리전
+	SetActorEnableCollision(InUse);
+	// 숨김
+	SetActorHiddenInGame(!InUse);
+}
+
+bool APooledActor::IsInUse() const
+{
+	return bInUse;
+}
+
+void APooledActor::BeginPlay()
+{
+	Super::BeginPlay();
+	SetInUse(false);
+}
+
+
