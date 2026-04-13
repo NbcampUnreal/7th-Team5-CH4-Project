06738cb122cce89d3c5015b34c7fa92eaa74210c
yoonseo <cysyes0403@naver.com>
2026-04-13 16:42:38 +0900

    feat: 다이아 엑터

---
+++ Source/DooDoong/Public/MiniGames/Catch/Actors/DiamondActor.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PooledActor.h"
+#include "DiamondActor.generated.h"
+
+UCLASS()
+class DOODOONG_API ADiamondActor : public APooledActor
+{
+	GENERATED_BODY()
+
+public:
+	ADiamondActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+};
