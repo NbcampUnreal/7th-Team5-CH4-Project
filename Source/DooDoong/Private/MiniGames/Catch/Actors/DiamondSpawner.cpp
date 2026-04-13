6e05d27b1645768a12bb5c37efe6e6407aaccf55
yoonseo <cysyes0403@naver.com>
2026-04-13 16:44:28 +0900

    feat: 다이아 스포너

---
+++ Source/DooDoong/Private/MiniGames/Catch/Actors/DiamondSpawner.cpp
@@ -0,0 +1,97 @@
+﻿#include "MiniGames/Catch/Actors/DiamondSpawner.h"
+
+#include "Common/DDLogManager.h"
+#include "MiniGames/Catch/Actors/PoolActorComponent.h"
+#include "Components/BoxComponent.h"
+#include "MiniGames/Catch/Actors/PooledActor.h"
+
+
+ADiamondSpawner::ADiamondSpawner()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SpawnBox = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawnBox"));
+	RootComponent = SpawnBox;
+
+	SpawnBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	bReplicates = true;
+	
+	Pool = CreateDefaultSubobject<UPoolActorComponent>(TEXT("Pool"));
+}
+
+void ADiamondSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ADiamondSpawner::SpawnDiamonds()
+{
+	if (!HasAuthority() || !Pool) return;
+
+	LOG_CYS(Warning, TEXT("[스포너] 다이아 스폰 시작"));
+	int32 Spawned = 0;
+
+	for (int32 i = 0; i < SpawnCount; i++)
+	{
+		FVector RandomLoc = GetRandomPointInBox();
+
+		FTransform Transform;
+		Transform.SetLocation(RandomLoc);
+
+		APooledActor* Actor = Pool->SpawnFromPool(Transform);
+
+		if (!Actor)
+		{
+			LOG_CYS(Error, TEXT("[스포너] 풀 부족"));
+			break; // 풀 부족
+		}
+
+		Spawned++;
+	}
+
+	// 부족하면 남은 것 전부 활성화
+	if (Spawned < SpawnCount)
+	{
+		for (APooledActor* Actor : Pool->ObjectPool)
+		{
+			if (Actor && !Actor->IsInUse())
+			{
+				Actor->SetInUse(true);
+			}
+		}
+	}
+	LOG_CYS(Warning, TEXT("[스포너] Spawned %d Diamonds"), SpawnCount);
+}
+
+FVector ADiamondSpawner::GetRandomPointInBox() const
+{
+	FVector Origin = SpawnBox->GetComponentLocation();
+	FVector Extent = SpawnBox->GetScaledBoxExtent();
+
+	return Origin + FVector(
+		FMath::RandRange(-Extent.X, Extent.X),
+		FMath::RandRange(-Extent.Y, Extent.Y),
+		FMath::RandRange(-Extent.Z, Extent.Z)
+	);
+}
+
+void ADiamondSpawner::StartSpawn()
+{
+	if (!HasAuthority()) return;
+	LOG_CYS(Error, TEXT("[스포너] StartSpawn"));
+
+	SpawnDiamonds();
+
+	GetWorldTimerManager().SetTimer(
+		SpawnTimer,
+		this,
+		&ADiamondSpawner::SpawnDiamonds,
+		SpawnInterval,
+		true
+	);
+}
+
+void ADiamondSpawner::StopSpawn()
+{
+	GetWorldTimerManager().ClearTimer(SpawnTimer);
+}
---
+++ Source/DooDoong/Public/MiniGames/Catch/Actors/DiamondSpawner.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DiamondSpawner.generated.h"
+
+class UBoxComponent;
+class UPoolActorComponent;
+UCLASS()
+class DOODOONG_API ADiamondSpawner : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ADiamondSpawner();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	UBoxComponent* SpawnBox;
+
+	UPROPERTY(VisibleAnywhere)
+	UPoolActorComponent* Pool;
+
+	UPROPERTY(EditAnywhere)
+	int32 SpawnCount = 5;
+
+	UPROPERTY(EditAnywhere)
+	float SpawnInterval = 10.f;
+
+	FTimerHandle SpawnTimer;
+
+	void SpawnDiamonds();
+	FVector GetRandomPointInBox() const;
+	
+public:
+	UFUNCTION()
+	void StartSpawn();
+
+	UFUNCTION()
+	void StopSpawn();
+};
