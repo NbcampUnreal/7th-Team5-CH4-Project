7a22ef16155c59636ed3d012e983d70c25ff330e
Daype <mj1527123@gmail.com>
2026-04-13 17:54:33 +0900

    feat : 아이템 버튼화 기능 추가

--- Source/DooDoong/Private/UI/Inventory/DDInvenGridSlot.cpp
+++ Source/DooDoong/Private/UI/Inventory/DDInvenGridSlot.cpp
@@ -2,8 +2,28 @@
 
 
 #include "UI/Inventory/DDInvenGridSlot.h"
+#include "Common/DDLogManager.h"
+#include "Components/Button.h"
 
-void UDDInvenGridSlot::SetImage(UImage* Image)
+void UDDInvenGridSlot::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if (BT_UseItem)
+	{
+		BT_UseItem->OnClicked.AddDynamic(this, &UDDInvenGridSlot::UseItem);
+		BT_UseItem->SetIsEnabled(bCanUse);
+	}
+}
+
+void UDDInvenGridSlot::SetItemInfo(UTexture2D* Image, int32 SetItemID, bool SetbCanUse)
 {
 	Image_ItemIcon = Image;
+	ItemID = SetItemID;
+	bCanUse = SetbCanUse;
+}
+
+void UDDInvenGridSlot::UseItem()
+{
+	LOG_PMJ(Warning, TEXT("OnClicked Event UseItem"));
 }
--- Source/DooDoong/Public/UI/Inventory/DDInvenGridSlot.h
+++ Source/DooDoong/Public/UI/Inventory/DDInvenGridSlot.h
@@ -6,21 +6,32 @@
 #include "Blueprint/UserWidget.h"
 #include "DDInvenGridSlot.generated.h"
 
-class UImage;
+class UTexture2D;
+class UButton;
 
 UCLASS()
 class DOODOONG_API UDDInvenGridSlot : public UUserWidget
 {
 	GENERATED_BODY()
 	
-public:
-	void SetImage(UImage* Image);
+protected:
+	virtual void NativeConstruct() override;
+	
+	void SetItemInfo(UTexture2D* Image, int32 ItemID, bool bCanUse);
+	
+	UFUNCTION()
+	void UseItem();
 	
 private:
 	UPROPERTY(meta=(BindWidget))
-	TObjectPtr<UImage> Image_ItemIcon;
+	TObjectPtr<UButton> BT_UseItem;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTexture2D> Image_ItemIcon;
 	
 	int32 ItemID;
 	
-	bool bOwned;
+	/* 버튼동작확인 임시 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "GridSlot|Bool", meta=(AllowPrivateAccess = true))
+	bool bCanUse;
 };
