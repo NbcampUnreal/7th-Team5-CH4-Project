02977aef877839b5134d608d6341114c27fbd648
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-04-14 13:41:57 +0900

    fix: 아이템 데이터에서 들고있던 메쉬를 TSubclassOf<AActor> DisplayActor로 변경
     요청에 의해 TSubclassOf<AActor> DisplayActor로 변경

--- Source/DooDoong/Public/Data/DDItemDataTypes.h
+++ Source/DooDoong/Public/Data/DDItemDataTypes.h
@@ -33,10 +33,5 @@ struct DOODOONG_API FItemTableRow : public FTableRowBase
 	
 	/** 아이템을 사용하면 갈아끼울 메쉬 */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TSoftObjectPtr<UStaticMesh> StaticMesh;
-	
-	/** 메쉬로 갈아끼우는게 불가능하면 액터를 붙히는 방식으로 교체 예정
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TSoftObjectPtr<AActor> MeshActor;
-	*/
+	TSubclassOf<AActor> DisplayActor;
 };
