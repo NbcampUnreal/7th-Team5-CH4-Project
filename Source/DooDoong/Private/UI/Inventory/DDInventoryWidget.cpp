d93106370072bf6f918053363b9e540f171bf83a
Daype <mj1527123@gmail.com>
2026-04-14 20:18:18 +0900

    refactor : 파일이름수정

---
+++ Source/DooDoong/Private/UI/Inventory/DDInventoryWidget.cpp
@@ -0,0 +1,74 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Inventory/DDInventoryWidget.h"
+
+#include "Base/Player/DDBasePlayerController.h"
+#include "UI/Inventory/DDInvenGridSlot.h"
+#include "Common/DDLogManager.h"
+#include "UI/Inventory/DDInventoryComponent.h"
+#include "Components/CanvasPanel.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Data/DDItemDataTypes.h"
+
+void UDDInventoryWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	ADDBasePlayerController* DDPC = Cast<ADDBasePlayerController>(GetOwningPlayer());
+	if (IsValid(DDPC) == true)
+	{
+		InventoryComponent = DDPC->GetComponentByClass<UDDInventoryComponent>();
+	}
+	Columns = InventoryComponent->InventoryItems.Num();
+	Rows = 1;
+	GenerationGrid();	
+}
+
+void UDDInventoryWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	RefreshGrid();
+}
+
+void UDDInventoryWidget::GenerationGrid()
+{
+	GridSlots.Reserve(Rows * Columns);
+	
+	for (int32 j = 0; j < Rows; ++j)
+	{
+		for (int32 i = 0; i < Columns; ++i)
+		{
+			UDDInvenGridSlot* GridSlot = CreateWidget<UDDInvenGridSlot>(this, GridSlotClass);
+			UCanvasPanelSlot* CanvasSlot = Cast<UCanvasPanelSlot>(CanvasPanel->AddChild(GridSlot));
+			
+			const float SlotSizeX = 200.f;
+			const float SlotSizeY = 150.f;
+			const float SpacingX = 100.f;
+			
+			if (CanvasSlot)
+			{
+				CanvasSlot->SetSize(FVector2D(SlotSizeX, SlotSizeY));
+				CanvasSlot->SetPosition(FVector2D(
+					i * (SlotSizeX + SpacingX) + 50.f,
+					j * (SlotSizeX) + 50.f
+					));
+			}
+			int32 Index = i + j * Columns;
+			if (!InventoryComponent->ItemNames.IsValidIndex(Index)) return;
+			FName ItemName = InventoryComponent->ItemNames[Index];
+			FItemTableRow* ItemData = InventoryComponent->GetItemData(ItemName);
+			GridSlot->SetItemInfo(ItemData);
+			GridSlots.Add(GridSlot);
+		}
+	}
+}
+
+void UDDInventoryWidget::RefreshGrid()
+{
+	LOG_PMJ(Error, TEXT("Call RefreshGrid"));
+	for (const auto& GridSlot : GridSlots)
+	{
+		GridSlot->UpdateItemInfo(InventoryComponent->InventoryItems);
+	}
+}
+
---
+++ Source/DooDoong/Public/UI/Inventory/DDInventoryWidget.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DDInventoryWidget.generated.h"
+
+class UDDInventoryComponent;
+class UCanvasPanel;
+class UDDInvenGridSlot;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnInventoryChanged);
+
+UCLASS()
+class DOODOONG_API UDDInventoryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeOnInitialized() override;
+	
+	virtual void NativeConstruct() override;
+	
+	void RefreshGrid();
+private:
+	void GenerationGrid();
+	
+	/* 인벤토리 그리드 슬롯 */
+	UPROPERTY()
+	TArray<UDDInvenGridSlot*> GridSlots;
+	
+	/* 인벤토리 컴포넌트 */
+	UPROPERTY()
+	TObjectPtr<UDDInventoryComponent> InventoryComponent;
+
+	/* 인벤토리 슬롯 클래스 */
+	UPROPERTY(EditDefaultsOnly, Category="Inventory")
+	TSubclassOf<UDDInvenGridSlot> GridSlotClass;
+	
+	/* 캔버스 패널 */
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UCanvasPanel> CanvasPanel;
+	
+	/* 인벤토리 슬롯개수*/
+	UPROPERTY(EditDefaultsOnly, Category="Inventory")
+	int32 Rows;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Inventory")
+	int32 Columns;
+	
+};
