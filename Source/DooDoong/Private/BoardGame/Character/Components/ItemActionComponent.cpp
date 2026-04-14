cb41142feb7fdff779baf3c5a66ec739b2852dbd
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-04-14 18:14:06 +0900

    feat: ItemActionComponent 추가
    ItemActionComponent 기반 확보

---
+++ Source/DooDoong/Private/BoardGame/Character/Components/ItemActionComponent.cpp
@@ -0,0 +1,62 @@
+﻿#include "BoardGame/Character/Components/ItemActionComponent.h"
+
+#include "GameplayTagContainer.h"
+#include "SEditorViewportToolBarMenu.h"
+#include "Data/DDItemDataTypes.h"
+#include "DSP/AudioDebuggingUtilities.h"
+#include "System/DDGameplayTags.h"
+
+UItemActionComponent::UItemActionComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UItemActionComponent::BeginItemAction(const FName ItemID)
+{
+	// TODO 아이템 사용을 시작
+	
+	FItemTableRow* Row = ItemData->FindRow<FItemTableRow>(ItemID, TEXT(""));
+	
+	if (!Row)
+	{
+		return;
+	}
+	
+	if (Row->ItemType == DDGameplayTags::Item_Activate_Instant)
+	{
+		ConfirmItemAction();
+	}
+	else
+	{
+		TryActivateItem();
+	}
+}
+
+void UItemActionComponent::TryActivateItem()
+{
+	// TODO 키 입력에 따른 카메라이동 구현. (이건 Tick???)
+	
+	
+}
+
+void UItemActionComponent::SelectNextTarget(AActor* Target)
+{
+	// TODO E를 누르면 다음 타겟으로 이동. 이동 텀이 있어야함. 0.5f초임
+}
+
+void UItemActionComponent::SelectPreviousTarget(AActor* Target)
+{
+	// TODO Q를 누르면 이전 타겟으로 이동
+}
+
+void UItemActionComponent::ConfirmItemAction()
+{
+	// TODO 아이템 사용을 Confirm
+}
+
+void UItemActionComponent::CancelItemAction()
+{
+	// TODO 아이템 사용을 Cancel
+	
+	// TODO 다시 인벤토리 창 띄우기
+}
---
+++ Source/DooDoong/Public/BoardGame/Character/Components/ItemActionComponent.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ItemActionComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class DOODOONG_API UItemActionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UItemActionComponent();
+	
+public:
+	void BeginItemAction(const FName ItemID);
+	
+	void TryActivateItem();
+	
+	void SelectNextTarget(AActor* Target);
+	
+	void SelectPreviousTarget(AActor* Target);
+	
+	void ConfirmItemAction();
+	
+	void CancelItemAction();
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ItemActionComp")
+	TObjectPtr<UDataTable> ItemData;
+	
+};
