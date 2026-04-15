1e8e36fe737f9538d4a56cd1ff2985330981cd5f
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-04-15 20:18:04 +0900

    feat: GiveBomb 아이템 전용 Ability 토대

---
+++ Source/DooDoong/Private/BoardGame/Abilities/ItemAbilities/GA_GiveBomb.cpp
@@ -0,0 +1,11 @@
+﻿#include "BoardGame/Abilities/ItemAbilities/GA_GiveBomb.h"
+
+#include "System/DDGameplayTags.h"
+
+UGA_GiveBomb::UGA_GiveBomb()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::ServerOnly;
+	
+	ActivationBlockedTags.AddTag(DDGameplayTags::State_Character_Death);
+}
