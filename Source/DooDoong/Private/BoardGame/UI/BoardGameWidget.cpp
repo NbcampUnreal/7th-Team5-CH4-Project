3d7e45b67872bd72e46be9b27288cfdec35dd76e
minsung <flymin00712@gmail.com>
2026-04-16 21:00:18 +0900

    feat : UI 로직 추가

--- Source/DooDoong/Private/BoardGame/UI/BoardGameWidget.cpp
+++ Source/DooDoong/Private/BoardGame/UI/BoardGameWidget.cpp
@@ -1,11 +1,16 @@
 #include "BoardGame/UI/BoardGameWidget.h"
-
-#include "BoardGame/Game/DDBoardGameMode.h"
+#include "BoardGame/Game/DDBoardGameState.h"
+#include "Components/TextBlock.h"
 
 void UBoardGameWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 	
+	ADDBoardGameState* GS = GetWorld()->GetGameState<ADDBoardGameState>();
+	if (!GS) return; 
+	
+	GS->OnStateTimerChanged.AddDynamic(this, &ThisClass::UpdateTimeText);
+	GS->OnRoundChanged.AddDynamic(this, &ThisClass::UpdateRemainTurn);
 }
 
 void UBoardGameWidget::NativeDestruct()
@@ -15,8 +20,18 @@ void UBoardGameWidget::NativeDestruct()
 
 void UBoardGameWidget::UpdateTimeText(int32 InTime)
 {
+	if (RemainTimeText)
+	{
+		FString TimeString = FString::Printf(TEXT("남은 시간 : %d"), InTime); 
+		RemainTimeText->SetText(FText::FromString(TimeString));
+	}
 }
 
 void UBoardGameWidget::UpdateRemainTurn(int32 InRemainTurn)
 {
+	if (RemainTurnText)
+	{
+		FString TurnString = FString::Printf(TEXT("남은 턴 : %d"), InRemainTurn);
+		RemainTurnText->SetText(FText::FromString(TurnString));
+	}
 }
