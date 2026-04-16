1ad6429e295b45c143354d824d8b74c7696c5335
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-04-16 14:31:45 +0900

    feat: Targeting 모드로 전환하는 어빌리티
    ItemActionComponent -> 타게팅 Ability
    - 타겟 후보 수집
    - Next/Previous/Confirm/Cancel 이벤트 처리
      - WaitGameplayEvent( ~, false)로 설정해서 입력을 여러 번 받을 수 있게 함
    - 이벤트 전송

--- Source/DooDoong/Public/BoardGame/Abilities/ItemAbilities/GA_Targeting.h
+++ Source/DooDoong/Public/BoardGame/Abilities/ItemAbilities/GA_Targeting.h
@@ -4,6 +4,8 @@
 #include "Abilities/GameplayAbility.h"
 #include "GA_Targeting.generated.h"
 
+class UItemActionComponent;
+
 UCLASS()
 class DOODOONG_API UGA_Targeting : public UGameplayAbility
 {
@@ -11,4 +13,46 @@ class DOODOONG_API UGA_Targeting : public UGameplayAbility
 
 public:
 	UGA_Targeting();
+
+	virtual void ActivateAbility(
+		FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
+
+	virtual void EndAbility(
+		FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled) override;
+
+protected:
+	UFUNCTION()
+	void OnTargetNext(FGameplayEventData Payload);
+
+	UFUNCTION()
+	void OnTargetPrevious(FGameplayEventData Payload);
+
+	UFUNCTION()
+	void OnTargetConfirm(FGameplayEventData Payload);
+
+	UFUNCTION()
+	void OnTargetCancel(FGameplayEventData Payload);
+
+	void BuildTargetCandidates();
+	void ChangeTarget(int32 Offset);
+	void FocusSelectedTarget();
+	void SendTargetResultEvent(const FGameplayTag& EventTag, AActor* TargetActor);
+	AActor* GetSelectedTarget() const;
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UItemActionComponent> ItemActionComponent;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> CandidateTargets;
+
+	UPROPERTY()
+	int32 SelectedTargetIndex = INDEX_NONE;
 };
