36956aeeb7033759ae6f1e42104a9ff14cf3f897
minsung <flymin00712@gmail.com>
2026-04-16 23:57:49 +0900

    feat: 로비 게임 스테이트 추가

--- Source/DooDoong/Private/Lobby/Game/DDLobbyGameState.cpp
+++ Source/DooDoong/Private/Lobby/Game/DDLobbyGameState.cpp
@@ -1,2 +1,30 @@
 #include "Lobby/Game/DDLobbyGameState.h"
+#include "Net/UnrealNetwork.h" 
 
+
+ADDLobbyGameState::ADDLobbyGameState()
+{
+}
+
+void ADDLobbyGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(ADDLobbyGameState, ReadyCount);
+	DOREPLIFETIME(ADDLobbyGameState, ParticipantCount);
+	DOREPLIFETIME(ADDLobbyGameState, CountDown);
+}
+
+void ADDLobbyGameState::OnRep_ParticipantCount()
+{
+}
+
+void ADDLobbyGameState::OnRep_ReadyCount()
+{
+	OnReadyCountChanged.Broadcast(ReadyCount);
+}
+
+void ADDLobbyGameState::OnRep_CountDown()
+{
+	OnCountDownChanged.Broadcast(CountDown);
+}
--- Source/DooDoong/Public/Lobby/Game/DDLobbyGameState.h
+++ Source/DooDoong/Public/Lobby/Game/DDLobbyGameState.h
@@ -5,11 +5,50 @@
 #include "DDLobbyGameState.generated.h"
 
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnReadyCountChanged, int32, ReadyCount);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnCountDownChanged, int32, CountDown);
+
+
 UCLASS()
 class DOODOONG_API ADDLobbyGameState : public AGameStateBase
 {
 	GENERATED_BODY()
 	
 public:
+	ADDLobbyGameState();
 	
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UFUNCTION()
+	void OnRep_ParticipantCount();
+	
+	UFUNCTION()
+	void OnRep_ReadyCount();
+	
+	UFUNCTION()
+	void OnRep_CountDown();
+	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Lobby")
+	int32 RequiredPlayerCount = 2; // 최소 게임 시작 인원 
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Lobby")
+	int32 MaxPlayerCount = 4; // 최대 게임 인원 
+	
+	UPROPERTY(ReplicatedUsing = OnRep_ReadyCount)
+	int32 ReadyCount = 0;
+	
+	UPROPERTY(ReplicatedUsing = OnRep_ParticipantCount)
+	int32 ParticipantCount = 0;
+	
+	UPROPERTY(ReplicatedUsing = OnRep_CountDown)
+	int32 CountDown = 0;
+	
+
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnReadyCountChanged OnReadyCountChanged;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnCountDownChanged OnCountDownChanged;
 };
