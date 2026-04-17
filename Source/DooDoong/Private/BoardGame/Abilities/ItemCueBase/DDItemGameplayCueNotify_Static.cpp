8b0d424486f50e907792970ff328a67c8d90ca23
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-04-17 17:16:06 +0900

    feat: GameplayCueNotify_Static 내부 로직 추가

--- Source/DooDoong/Private/BoardGame/Abilities/ItemCueBase/DDItemGameplayCueNotify_Static.cpp
+++ Source/DooDoong/Private/BoardGame/Abilities/ItemCueBase/DDItemGameplayCueNotify_Static.cpp
@@ -1,2 +1,90 @@
 #include "BoardGame/Abilities/ItemCueBase/DDItemGameplayCueNotify_Static.h"
 
+#include "Animation/AnimInstance.h"
+#include "Animation/AnimSequenceBase.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "NiagaraFunctionLibrary.h"
+
+bool UDDItemGameplayCueNotify_Static::OnExecute_Implementation(
+	AActor* MyTarget,
+	const FGameplayCueParameters& Parameters) const
+{
+	Super::OnExecute_Implementation(MyTarget, Parameters);
+
+	PlayAnimation(Parameters);
+	PlaySound(MyTarget, Parameters);
+	SpawnEffect(MyTarget, Parameters);
+
+	return true;
+}
+
+void UDDItemGameplayCueNotify_Static::PlayAnimation(const FGameplayCueParameters& Parameters) const
+{
+	if (!Animation)
+	{
+		return;
+	}
+
+	AActor* Instigator = Parameters.GetInstigator();
+	if (!Instigator)
+	{
+		return;
+	}
+
+	USkeletalMeshComponent* MeshComponent = Instigator->FindComponentByClass<USkeletalMeshComponent>();
+	if (!MeshComponent)
+	{
+		return;
+	}
+
+	UAnimInstance* AnimInstance = MeshComponent->GetAnimInstance();
+	if (!AnimInstance)
+	{
+		return;
+	}
+
+	AnimInstance->PlaySlotAnimationAsDynamicMontage(
+		Animation,
+		AnimationSlotName,
+		BlendInTime,
+		BlendOutTime,
+		PlayRate
+	);
+}
+
+void UDDItemGameplayCueNotify_Static::PlaySound(
+	const UObject* WorldContextObject,
+	const FGameplayCueParameters& Parameters) const
+{
+	if (!Sound || !WorldContextObject)
+	{
+		return;
+	}
+
+	UGameplayStatics::PlaySoundAtLocation(
+		WorldContextObject,
+		Sound,
+		Parameters.Location,
+		SoundVolumeMultiplier,
+		SoundPitchMultiplier
+	);
+}
+
+void UDDItemGameplayCueNotify_Static::SpawnEffect(
+	const UObject* WorldContextObject,
+	const FGameplayCueParameters& Parameters) const
+{
+	if (!Effect || !WorldContextObject)
+	{
+		return;
+	}
+
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+		WorldContextObject,
+		Effect,
+		Parameters.Location,
+		FRotator::ZeroRotator,
+		EffectScale
+	);
+}
