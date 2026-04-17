3c3b2ed19007e7306b7ff2ecb62d2319c6484f8e
Joung_Juhyeon <wjdwngus2056@gmailc.om>
2026-04-17 12:10:31 +0900

    feat: 아이템 어빌리티 베이스에 몽타주 재생 패턴 추가

--- Source/DooDoong/Public/BoardGame/Abilities/ItemAbilities/ItemBase/GA_ItemBase.h
+++ Source/DooDoong/Public/BoardGame/Abilities/ItemAbilities/ItemBase/GA_ItemBase.h
@@ -6,6 +6,8 @@
 
 class ADDBoardGameCharacter;
 class UAbilitySystemComponent;
+class UAbilityTask_PlayMontageAndWait;
+class UAnimMontage;
 
 UCLASS(Abstract)
 class DOODOONG_API UGA_ItemBase : public UGameplayAbility
@@ -24,7 +26,25 @@ public:
 protected:
 	/** 아이템을 사용하는 보드게임 캐릭터 Getter */
 	ADDBoardGameCharacter* GetBoardGameCharacter() const;
-protected:
+
 	/** 보드게임 캐릭터가 사용하는 ASC Getter */
 	UAbilitySystemComponent* GetBoardGameAbilitySystemComponent() const;
+
+	bool PlayItemMontage(float PlayRate = 1.f, FName StartSection = NAME_None);
+
+	UFUNCTION()
+	virtual void OnItemMontageCompleted();
+
+	UFUNCTION()
+	virtual void OnItemMontageInterrupted();
+
+	UFUNCTION()
+	virtual void OnItemMontageCancelled();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item|Animation")
+	TObjectPtr<UAnimMontage> ItemMontage;
+
+	UPROPERTY()
+	TObjectPtr<UAbilityTask_PlayMontageAndWait> ActiveItemMontageTask;
 };
