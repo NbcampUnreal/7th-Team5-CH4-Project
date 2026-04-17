69127279acd77786acb5bb4d6946c34e2fb1eaab
ChoiJunHyeok <optimal9744@gmail.com>
2026-04-17 13:11:48 +0900

    fix: BoardGameWidget UI 연동 - 네트워크 레이스 컨디션 문제 해결
    [문제 원인]
    1. 심리스 트래블 직후 클라이언트에서 BoardGameWidget 생성
    2. 네트워크를 통해 BoardGameState가 클라이언트에 도착하지 않은 상태라 NULL 상태로, 바인딩 실패 → 이 시점에서 Broadcast 의미 없음
    
    [해결]
    - BoardGameState를 불러올 때까지 0.1초마다 바인딩 시도

--- Source/DooDoong/Public/BoardGame/UI/BoardGameWidget.h
+++ Source/DooDoong/Public/BoardGame/UI/BoardGameWidget.h
@@ -4,6 +4,7 @@
 #include "Blueprint/UserWidget.h"
 #include "BoardGameWidget.generated.h"
 
+class ADDBoardGameState;
 class UBoardGamePlayerInfo;
 class UTextBlock;
 
@@ -16,7 +17,8 @@ public:
 	virtual void NativeConstruct() override;
 	
 	virtual void NativeDestruct() override;
-	
+	void BindToGameState();
+
 	UFUNCTION()
 	void UpdateTimeText(int32 InTime);
 	
@@ -33,5 +35,8 @@ protected:
 	
 	// UPROPERTY(meta =(BindWidget))
 	// TArray<UBoardGamePlayerInfo*> PlayerInfos;
+	
+	UPROPERTY()
+	TObjectPtr<ADDBoardGameState> CurrentGameState;
 	
 };
