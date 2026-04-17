63e5f3cdf97a17d0c8a5c8c23c4e5a4d6f86329c
yoonseo <cysyes0403@naver.com>
2026-04-17 15:03:20 +0900

    feat: 자석 아이템

--- Source/DooDoong/Private/BoardGame/Abilities/ItemAbilities/GA_Magnet.cpp
+++ Source/DooDoong/Private/BoardGame/Abilities/ItemAbilities/GA_Magnet.cpp
@@ -1,4 +1,21 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "BoardGame/Abilities/ItemAbilities/GA_Magnet.h"
+#include "AbilitySystemComponent.h"
+#include "Common/DDLogManager.h"
 
+void UGA_Magnet::OnConfirm(FGameplayEventData Payload)
+{
+	Super::OnConfirm(Payload);
+	
+	UAbilitySystemComponent* SourceASC = GetAbilitySystemComponentFromActorInfo();
+	if (!SourceASC) return;
 
-#include "BoardGame/Abilities/ItemAbilities/GA_Magnet.h"
+	FGameplayEffectSpecHandle SpecHandle = MakeOutgoingGameplayEffectSpec(EffectClass, 1.f);
+	if (!SpecHandle.IsValid()) return;
+
+	SpecHandle.Data->SetSetByCallerMagnitude(EffectDataTag, TotalDrainedAmount);
+	
+	// 코인 증가
+	SourceASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data);
+	LOG_CYS(Warning, TEXT("UGA_Magnet: Successfully applied Magnet"));
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
--- Source/DooDoong/Public/BoardGame/Abilities/ItemAbilities/GA_Magnet.h
+++ Source/DooDoong/Public/BoardGame/Abilities/ItemAbilities/GA_Magnet.h
@@ -1,16 +1,14 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
-#include "Abilities/GameplayAbility.h"
+#include "ItemBase/GA_RangeItemBase.h"
 #include "GA_Magnet.generated.h"
 
-/**
- * 
- */
 UCLASS()
-class DOODOONG_API UGA_Magnet : public UGameplayAbility
+class DOODOONG_API UGA_Magnet : public UGA_RangeItemBase
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void OnConfirm(FGameplayEventData Payload) override;
 };
