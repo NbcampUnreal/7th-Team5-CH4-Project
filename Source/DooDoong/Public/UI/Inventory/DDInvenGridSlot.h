b702f6731dd2efe73505166882f97f78a34947bb
Daype <mj1527123@gmail.com>
2026-04-19 22:36:50 +0900

    refactor : 그리드슬롯 데이터 반영구조 리팩토링

--- Source/DooDoong/Public/UI/Inventory/DDInvenGridSlot.h
+++ Source/DooDoong/Public/UI/Inventory/DDInvenGridSlot.h
@@ -6,8 +6,8 @@
 #include "Blueprint/UserWidget.h"
 #include "DDInvenGridSlot.generated.h"
 
-struct FInventoryItemData;
-struct FItemTableRow;
+class UDDInventoryWidget;
+struct FViewItemData;
 class UTextBlock;
 class UImage;
 class UButton;
@@ -19,19 +19,21 @@ class DOODOONG_API UDDInvenGridSlot : public UUserWidget
 	GENERATED_BODY()
 	
 public:
-	void SetItemInfo(const FItemTableRow& ItemTableRow);
-	
-	void UpdateItemInfo(const FInventoryItemData& InventoryItemData);
-	
-protected:
 	virtual void NativeConstruct() override;
+	
+	void SetItemInfo(const FViewItemData& ViewItemData);
+	
+	void UpdateItemInfo(const FViewItemData& ViewItemData);
 	
 	UFUNCTION()
 	void UseItem();
 	
-private:
 	UPROPERTY(meta=(BindWidget))
 	TObjectPtr<UButton> BT_ClickItem;
+	
+private:
+	/* 그리드슬롯 그리는 UI변수 */
+	
 	
 	UPROPERTY(meta=(BindWidget))
 	TObjectPtr<UImage> Image_ItemIcon;
@@ -39,12 +41,14 @@ private:
 	UPROPERTY(meta=(BindWidget))
 	TObjectPtr<UTextBlock> TextBlock_ItemCount;
 	
+	/* 그리드슬롯 클릭시 그려지는 UI위젯 변수 */
 	UPROPERTY()
 	TObjectPtr<UDDItemUseButtonWidget> ItemUseButtonWidget;
 	
 	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
 	TSubclassOf<UDDItemUseButtonWidget> ItemUseButtonWidgetClass;
 	
+	/* 그리드슬롯 그리기 내부 데이터 */
 	FName ItemName;
 	int32 ItemCount;
 	bool bCanUse;
