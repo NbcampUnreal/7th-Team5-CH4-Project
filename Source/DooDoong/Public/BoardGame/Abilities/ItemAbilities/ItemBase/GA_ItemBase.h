8cb3692070be6d322dbf80ffaecbe2365247ac9b
yoonseo <cysyes0403@naver.com>
2026-04-19 22:39:17 +0900

    feat: 아이템 사용 시 아이템 스폰
    ^ Conflicts:
    ^	Source/DooDoong/Public/BoardGame/Abilities/ItemAbilities/ItemBase/GA_ItemBase.h

--- Source/DooDoong/Public/BoardGame/Abilities/ItemAbilities/ItemBase/GA_ItemBase.h
+++ Source/DooDoong/Public/BoardGame/Abilities/ItemAbilities/ItemBase/GA_ItemBase.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "Abilities/GameplayAbility.h"
+#include "Data/DDItemDataTypes.h"
 #include "GameplayTagContainer.h"
 #include "GA_ItemBase.generated.h"
 
@@ -22,13 +23,28 @@ public:
 		FGameplayAbilityActivationInfo ActivationInfo,
 		const FGameplayEventData* TriggerEventData) override;
 
+	virtual void EndAbility(
+		FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled
+	) override;
+	
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item|Cue")
 	FGameplayTag ConfirmCueTag;
 
 	ADDBoardGameCharacter* GetBoardGameCharacter() const;
 
+	/** 보드게임 캐릭터가 사용하는 ASC Getter */
 	UAbilitySystemComponent* GetBoardGameAbilitySystemComponent() const;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "UX")
+	AActor* ItemActor;
+	
+	UPROPERTY()
+	FItemTableRow CachedItemData;
 
 	bool ExecuteItemCue(const FGameplayTag& CueTag) const;
 	bool ExecuteItemCueOnTarget(const FGameplayTag& CueTag, AActor* CueActor) const;
