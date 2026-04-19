827363eddccd6899c44c0f2d49e2b4cc3caa2836
yoonseo <cysyes0403@naver.com>
2026-04-19 22:34:26 +0900

    feat: 아이템 Payload

---
+++ Source/DooDoong/Public/Data/ItemPayloadObject.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "DDItemDataTypes.h"
+#include "ItemPayloadObject.generated.h"
+
+UCLASS()
+class UItemPayloadObject : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	FItemTableRow ItemRow;
+};
