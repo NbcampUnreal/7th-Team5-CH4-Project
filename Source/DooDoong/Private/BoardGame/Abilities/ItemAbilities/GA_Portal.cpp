c4856c01177b778432827f0479e6ba885490a40d
yoonseo <cysyes0403@naver.com>
2026-04-20 11:45:44 +0900

    feat: 포탈 아이템 GA

---
+++ Source/DooDoong/Private/BoardGame/Abilities/ItemAbilities/GA_Portal.cpp
@@ -0,0 +1,45 @@
+﻿#include "BoardGame/Abilities/ItemAbilities/GA_Portal.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Base/Player/DDBasePlayerState.h"
+#include "BoardGame/DDTile.h"
+#include "BoardGame/Character/DDBoardGameCharacter.h"
+#include "Common/DDLogManager.h"
+
+bool UGA_Portal::ExecuteTargetingItem(AActor* TargetActor)
+{
+	ADDBoardGameCharacter* CasterCharacter = GetBoardGameCharacter();
+	if (!IsValid(CasterCharacter)) return false;
+	ADDBoardGameCharacter* TargetCharacter = Cast<ADDBoardGameCharacter>(TargetActor);
+	if (!IsValid(TargetCharacter)) return false;
+	
+	// 캐스터 플레이어 state
+	ADDBasePlayerState* CasterState = CasterCharacter->GetPlayerState<ADDBasePlayerState>();
+	// 타겟 플레이어 state
+	ADDBasePlayerState* TargetState = TargetCharacter->GetPlayerState<ADDBasePlayerState>();
+	if (!CasterState)
+	{
+		LOG_CYS(Warning, TEXT("캐스터 PS 없음"));
+		return false;
+	}
+	if (!TargetState)
+	{
+		LOG_CYS(Warning, TEXT("타겟 PS 없음"));
+		return false;
+	}
+	// CurrentTile 값 swap
+	if (!CasterState->CurrentTile) return false;
+	if (!TargetState->CurrentTile) return false;
+	
+	auto TempTile = CasterState->CurrentTile;
+	CasterState->CurrentTile = TargetState->CurrentTile;
+	TargetState->CurrentTile = TempTile;
+	
+	// 위치 이동
+	CasterCharacter->SetActorLocation(CasterState->CurrentTile->GetStandLocation(CasterCharacter));
+	TargetCharacter->SetActorLocation(TargetState->CurrentTile->GetStandLocation(TargetCharacter));
+	
+	LOG_CYS(Warning,TEXT("포탈 아이템: 자리 교환 완료"));
+	
+	return true;
+}
