7159a803755c30e6e236318a70e24409218e5dcc
ChoiJunHyeok <optimal9744@gmail.com>
2026-04-21 19:41:38 +0900

    build: 패키징 설정

---
+++ Source/DooDoongEditorServer.Target.cs
@@ -0,0 +1,21 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class DooDoongServerTarget : TargetRules
+{
+	public DooDoongServerTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		ExtraModuleNames.Add("DooDoong");
+		RegisterModulesCreatedByRider();
+	}
+
+	private void RegisterModulesCreatedByRider()
+	{
+		ExtraModuleNames.AddRange(new string[] { "DDGitStatusBranch" });
+	}
+}
