270d6d1137a81957feff3d07018bed841b6a26b1
Daype <mj1527123@gmail.com>
2026-04-22 19:38:03 +0900

    refactor : 플랫포머 점수계산로직 리팩토링

--- Source/DooDoong/Public/MiniGames/Platformer/GameMode/DDPlatformerGameMode.h
+++ Source/DooDoong/Public/MiniGames/Platformer/GameMode/DDPlatformerGameMode.h
@@ -4,6 +4,7 @@
 #include "Base/MiniGame/DDMiniGameModeBase.h"
 #include "DDPlatformerGameMode.generated.h"
 
+class ADDPlatformerCharacter;
 class ADDPlatformerGameState;
 class ADDBasePlayerState;
 class ADDBasePlayerController;
@@ -53,15 +54,25 @@ class DOODOONG_API ADDPlatformerGameMode : public ADDMiniGameModeBase
 {
 	GENERATED_BODY()
 public:
+	/* 초기화 함수 */
+	virtual void HandleSeamlessTravelPlayer(AController*& C) override;
+	
 	virtual void BeginPlay() override;
 	
 	virtual void StartMiniGame() override;
-	
+
 public:
+	/* 게임진행 관련 함수 */
+	void SetEnteredPlayer();
+	
+	void CheckGoalPlayerCharacter(AActor* OverlapActor);
+	
+	void ReturnToSavePoint(AActor* OverlapActor);
+	
+	void UpdatePlayerSavePoint(AActor* OverlapActor, const FVector UpdateSavePoint);
+	
 	/* 플레이어 이동거리 체크 타이머 함수 */
 	void PlayGameTimer();
-	
-	void CheckGoalPlayerCharacter(AActor* GoalActor);
 	
 	void PlayerRanking();
 	
@@ -72,12 +83,7 @@ public:
 	
 	void CallFinishGameTimer();
 	
-	/* 초기화 함수 */
-	virtual void HandleSeamlessTravelPlayer(AController*& C) override;
-public:
-	/* 참가한 플레이어 관련 정보 */
-	UPROPERTY(VisibleAnywhere, Category = "PlatformerData | EnteredPlayer")
-	TMap<int32, FPlatformerPlayerData> PlayerDatas;
+	
 	
 protected:
 	UPROPERTY(EditDefaultsOnly, Category = "PlatformerData")
@@ -85,14 +91,13 @@ protected:
 	
 	UPROPERTY(EditDefaultsOnly, Category = "PlatformerData")
 	TObjectPtr<UInputMappingContext> PlatformerIMC;
-	
 	/* 참가한 플레이어 관련 정보 */
 	
-	UPROPERTY(VisibleAnywhere, Category = "PlatformerData | EnteredPlayer")
-	TMap<int32, FPlatformerPlayerData> PlayerRankingArrays;
+	UPROPERTY(VisibleAnywhere, Category = "PlatformerData | EnteredCharacter")
+	TArray<TObjectPtr<ADDPlatformerCharacter>> EnteredPlayers;
 	
-	UPROPERTY(VisibleAnywhere, Category = "PlatformerData | EnteredPlayer")
-	TMap<int32, FPlatformerPlayerData> PlayerNoGoalArrays;
+	UPROPERTY(VisibleAnywhere, Category = "PlatformerData | EnteredCharacter")
+	TArray<TObjectPtr<ADDPlatformerCharacter>> NoGoalPlayers;
 	
 	/* 게임 진행시간 */
 	UPROPERTY(EditAnywhere, Category = "PlatformerData")
@@ -104,14 +109,12 @@ protected:
 	
 private:
 	/* 타이머핸들러 */
-	FTimerHandle FinishedWaitingTimerHandle;
 	FTimerHandle DistanceTimerHandle;
 	FTimerHandle PlatformerPlayTimerHandle;
 	FTimerHandle FinishGameTimerHandle;
 	
 	/* 게임중인 플레이어 관련 변수 */
-	FVector StartLocation;
+	FVector2D StartLocation;
 	int32 Rank = 1;
-	int32 MaxPlayer;
 	
 };
