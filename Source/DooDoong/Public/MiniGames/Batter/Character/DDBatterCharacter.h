867d8a429df74bdf628170ffb8c4a005b13a8155
yoonseo <cysyes0403@naver.com>
2026-04-22 12:18:27 +0900

    feat: 배터 캐릭터

---
+++ Source/DooDoong/Public/MiniGames/Batter/Character/DDBatterCharacter.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/Character/DDBaseCharacter.h"
+#include "DDBatterCharacter.generated.h"
+
+class UInputAction;
+
+UCLASS()
+class DOODOONG_API ADDBatterCharacter : public ADDBaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ADDBatterCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	UFUNCTION(Server, Reliable)
+	void Server_AddScore();
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bPressing;
+	
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	const UInputAction* IA_Bat;
+	
+	UFUNCTION()
+	void OnPressSpace();
+	
+	UFUNCTION()
+	void OnReleaseSpace();
+	
+};
